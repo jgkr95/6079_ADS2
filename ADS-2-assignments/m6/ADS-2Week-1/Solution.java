e3df48c806c90040b4f7c77bc61fa676ad63bf3d
jgkr95 <jgkr95@msitprogram.net>
2018-11-03 12:49:51 +0530

    "ads2_m6_a1_1: 3/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m6/ADS-2Week-1/Solution.java
+++ ADS-2-assignments/m6/ADS-2Week-1/Solution.java
@@ -1,89 +1,131 @@
 
 import java.util.Scanner;
-import java.io.File;
+/**
+ * Class for page rank.
+ */
 class PageRank {
-	double[] pr;
-	Iterable<Integer> incoming;
+    /**
+     * page rank array.
+     */
+    double[] pr;
+    /**
+     * Incoming edges.
+     */
+    Iterable<Integer> incoming;
+    /**
+     * Constructs the object and calculates values of pagerank array.
+     * Time complexity is O(V*1000*E).
+     * @param      dg    { Digraph object }
+     */
+    PageRank(Digraph dg) {
+        Digraph dgh = dg;
+        Digraph dgRev = new Digraph(dg.V());
+        dgRev = dg.reverse();
 
-	PageRank(Digraph dg) {
-		Digraph dgh = dg;
-		Digraph dgRev = new Digraph(dg.V());
-		dgRev = dg.reverse();
-
-		pr = new double[dg.V()];
-		for (int i = 0; i < dg.V(); i++) {
-			pr[i] = (double) 1 / dg.V();
-			// incoming=dgRev.adj(i);
-			// for(int j=0;j<1000;j++) {
-			// 	for(Integer w:incoming) {
-			// 		pr[i]+=pr[w]+dgh.outdegree(w);
-			// 	}
-			// }
-		}
-		for (int i = 0; i < dg.V(); i++) {
-			// pr[i]=1/dg.V();
-			incoming = dgRev.adj(i);
-			for (int j = 0; j < 1000; j++) {
-				for (Integer w : incoming) {
-					// pr[i]+=pr[w]+dgh.outdegree(w);
-				}
-			}
-		}
-	}
-	public double getPR(int v) {
-		return pr[v];
-	}
-	public String toString() {
-		String str = "";
-		for (int i = 0; i < pr.length; i++) {
-			str += i + " - " + pr[i] + "\n";
-		}
-		return str;
-	}
+        pr = new double[dg.V()];
+        for (int i = 0; i < dg.V(); i++) {
+            pr[i] = (double) 1 / dg.V();
+            // incoming=dgRev.adj(i);
+            // for(int j=0;j<1000;j++) {
+            //  for(Integer w:incoming) {
+            //      pr[i]+=pr[w]+dgh.outdegree(w);
+            //  }
+            // }
+        }
+        for (int i = 0; i < dg.V(); i++) {
+            // pr[i]=1/dg.V();
+            incoming = dgRev.adj(i);
+            for (int j = 0; j < 1000; j++) {
+                for (Integer w : incoming) {
+                    // pr[i]+=pr[w]+dgh.outdegree(w);
+                }
+            }
+        }
+    }
+    /**
+     * Gets the pr value of given index.
+     * Time complexity is O(1).
+     * @param      v     { parameter_description }
+     *
+     * @return     The pr.
+     */
+    public double getPR(int v) {
+        return pr[v];
+    }
+    /**
+     * Returns a string representation of the object.
+     * Time complexity is O(V).
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        String str = "";
+        for (int i = 0; i < pr.length; i++) {
+            str += i + " - " + pr[i] + "\n";
+        }
+        return str;
+    }
 }
-
+/**
+ * Class for web search.
+ */
 class WebSearch {
-
+    /**
+     * Constructs the object.
+     */
+    private WebSearch() { }
 }
 
-
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		// read the first line of the input to get the number of vertices
-		int vertices = Integer.parseInt(scan.nextLine());
-		Digraph dg = new Digraph(vertices);
-		while (scan.hasNext()) {
-			String[] tokens = scan.nextLine().split(" ");
-			for (int i = 1; i < tokens.length; i++) {
-				dg.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[i]));
-			}
-		}
-		System.out.println(dg);
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * { main method to handle the input }.
+     * Time complexity is O(V*1000*E).
+     * @param      args  The arguments are String array type
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // read the first line of the input to get the number of vertices
+        int vertices = Integer.parseInt(scan.nextLine());
+        Digraph dg = new Digraph(vertices);
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(" ");
+            for (int i = 1; i < tokens.length; i++) {
+                dg.addEdge(Integer.parseInt(tokens[0]),
+                 Integer.parseInt(tokens[i]));
+            }
+        }
+        System.out.println(dg);
 
 
-		// iterate count of vertices times
-		// to read the adjacency list from std input
-		// and build the graph
+        // iterate count of vertices times
+        // to read the adjacency list from std input
+        // and build the graph
 
 
-		// Create page rank object and pass the graph object to the constructor
-		PageRank pr = new PageRank(dg);
-		// print the page rank object
-		System.out.println(pr);
+        // Create page rank object and pass the graph object to the constructor
+        PageRank pr = new PageRank(dg);
+        // print the page rank object
+        System.out.println(pr);
 
-		// This part is only for the final test case
+        // This part is only for the final test case
 
-		// File path to the web content
-		String file = "WebContent.txt";
+        // File path to the web content
+        String file = "WebContent.txt";
 
-		// instantiate web search object
-		// and pass the page rank object and the file path to the constructor
+        // instantiate web search object
+        // and pass the page rank object and the file path to the constructor
 
-		// read the search queries from std in
-		// remove the q= prefix and extract the search word
-		// pass the word to iAmFeelingLucky method of web search
-		// print the return value of iAmFeelingLucky
+        // read the search queries from std in
+        // remove the q= prefix and extract the search word
+        // pass the word to iAmFeelingLucky method of web search
+        // print the return value of iAmFeelingLucky
 
-	}
+    }
 }
