68f071d3dd72e515b1f75ec05f576b14a03f15d8
jgkr95 <jgkr95@msitprogram.net>
2018-11-10 10:59:20 +0530

    "ads2_m12_a1_1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
@@ -32,11 +32,11 @@ public class Solution {
             // Other wise print "No Path Found."
             String[] paths = scan.nextLine().split(" ");
             DijkstraSP d = new DijkstraSP(ewg, Integer.parseInt(paths[0]));
-            if ((int) d.distance(Integer.parseInt(paths[1])) == 0) {
-                System.out.println("No Path Found.");
-            } else {
+            // if ((int) d.distance(Integer.parseInt(paths[1])) == 0) {
+            //     System.out.println("No Path Found.");
+            // } else {
                 System.out.format("%.1f", (int) d.distance(Integer.parseInt(paths[1])));
-            }       break;
+                 break;
 
         case "ViaPaths":
             // Handle the case of ViaPaths, where three integers are given.
