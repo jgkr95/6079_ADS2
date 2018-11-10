b6978259fa331fd61f60490d5a6e6150940455ef
jgkr95 <jgkr95@msitprogram.net>
2018-11-10 12:08:37 +0530

    "ads2_m12_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
 public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-    public static void main(String[] args) {
+    }
+    /**
+     * Main method to handle to input.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         // Self loops are not allowed...
         // Parallel Edges are allowed...
         // Take the Graph input here...
@@ -31,44 +41,47 @@ public class Solution {
             // If the path exists print the distance between them.
             // Other wise print "No Path Found."
             String[] paths = scan.nextLine().split(" ");
-            DijkstraUndirectedSP d = new DijkstraUndirectedSP(ewg, Integer.parseInt(paths[0]));
+            DijkstraUndirectedSP d = new
+             DijkstraUndirectedSP(ewg, Integer.parseInt(paths[0]));
             if (!d.hasPathTo(Integer.parseInt(paths[1]))) {
                 System.out.println("No Path Found.");
             } else {
-                System.out.format("%.1f", d.distTo(Integer.parseInt(paths[1])));
+                System.out.format("%.1f",
+                 d.distTo(Integer.parseInt(paths[1])));
             }
             break;
 
         case "ViaPaths":
             // Handle the case of ViaPaths, where three integers are given.
-            // First is the source and second is the via is the one where path should pass throuh.
+            // First is the source and second is the via is the
+            //  one where path should pass throuh.
             // third is the destination.
             // If the path exists print the distance between them.
             // Other wise print "No Path Found."
             String[] tokens2 = scan.nextLine().split(" ");
-      DijkstraUndirectedSP sp1 = new DijkstraUndirectedSP(
-        ewg, Integer.parseInt(tokens2[0]));
-      DijkstraUndirectedSP sp2 = new DijkstraUndirectedSP(
-        ewg, Integer.parseInt(tokens2[1]));
-      String str = "";
-      if (sp1.hasPathTo(Integer.parseInt(tokens2[2]))) {
-        int i = 0;
-        System.out.println(sp1.distTo(Integer.parseInt(tokens2[1]))
-         + sp2.distTo(Integer.parseInt(tokens2[2])));
-        for (Integer e : sp1.pathTo(Integer.parseInt(tokens2[1]))) {
-          str += e + " ";
-        }
-        for (Integer e : sp2.pathTo(Integer.parseInt(tokens2[2]))) {
-          if (i != 0) {
-            str += e + " ";
-          }
-          i++;
-        }
-        System.out.println(str.substring(0, str.length() - 1));
-      } else {
-        System.out.println("No Path Found.");
-      }
-      break;
+            DijkstraUndirectedSP sp1 = new DijkstraUndirectedSP(
+                ewg, Integer.parseInt(tokens2[0]));
+            DijkstraUndirectedSP sp2 = new DijkstraUndirectedSP(
+                ewg, Integer.parseInt(tokens2[1]));
+            String str = "";
+            if (sp1.hasPathTo(Integer.parseInt(tokens2[2]))) {
+                int i = 0;
+                System.out.println(sp1.distTo(Integer.parseInt(tokens2[1]))
+                                   + sp2.distTo(Integer.parseInt(tokens2[2])));
+                for (Integer e : sp1.pathTo(Integer.parseInt(tokens2[1]))) {
+                    str += e + " ";
+                }
+                for (Integer e : sp2.pathTo(Integer.parseInt(tokens2[2]))) {
+                    if (i != 0) {
+                        str += e + " ";
+                    }
+                    i++;
+                }
+                System.out.println(str.substring(0, str.length() - 1));
+            } else {
+                System.out.println("No Path Found.");
+            }
+            break;
         default:
             break;
         }
